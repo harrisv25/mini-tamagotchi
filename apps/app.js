730afa8656e28496ea254adaa2a1f5104a28ff09
harrisv25 <harrisv25@gmail.com>
2022-07-06 17:07:27 -0600

    Tamagotchi object created

--- apps/app.js
+++ apps/app.js
@@ -1,1 +1,9 @@
-console.log("this works")
+class Creature {
+    constructor(name) {
+        this.name = name
+        this.hunger = 1
+        this.sleepiness = 1
+        this.boredom = 1
+        Age = 0
+    }
+}
