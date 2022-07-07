c1eb327aab4405ad676a59d0ea680afb2e832da2
harrisv25 <harrisv25@gmail.com>
2022-07-06 20:05:09 -0600

    Added the rest of the icons as well as the animated monster

--- apps/app.js
+++ apps/app.js
@@ -16,9 +16,13 @@ let pet = {}
 function createPet (name) {
     let myPet = document.createElement("div")
     myPet.setAttribute('id', name)
-    myPet.setAttribute('class', "egg")
+    myPet.setAttribute('class', "pet")
+    let myEgg = document.createElement("img")
+    myEgg.setAttribute('src', "/img/demon_cat.webp")
+    myPet.appendChild(myEgg)
     document.querySelector('#center-piece').appendChild(myPet)
     document.querySelector('#starter').parentNode.removeChild(document.querySelector('#starter'))
+
     pet = new Creature(name)
 }
 
